07f352e8c4b3fa7287fd94f1b12c2bb0b9be3c83
JoaoAugustoMPdJ <103338792+JoaoAugustoMPdJ@users.noreply.github.com>
2023-12-18 12:24:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function openPresent() {
+  var letterContainer = document.getElementById("letter-container");
+
+  // Simulação de uma carta personalizada
+  var carta = `
+    <p>Para minha Jujubinha,</p>
+    <p>Feliz Natal e um próspero Ano Novo!</p>
+    <p>Tô escrevendo isso como um presente para você, quero agradecer por tudo,
+       todos os momentos mesmo. Peço desculpas pelas vezes que te irritei e saibe que vou continuar fazendo isso,
+       não quero me afastar de você, então tu vai ter que suportar. Tô escrevendo tudo isso do coração, 
+       então também peço desculpas pela falta de linearidade e sentido as vezes. Te amo demais Janine, 
+       gostaria de poder te abraçar e te beijar, tô escrevendo tudo isso porque você é especial pra mim,
+       é dificil por tudo isso em palavras mas eu tento demais, guarda isso com carinho e no coração
+       as palavras que aqui estiverem. Tenho milhares de motivos para te amar, você é incrivel, maravilhosa
+       divertida, carinhosa, chata, teimosa tudo isso me diverte e me deixa feliz, pode até parecer, clichê e brega mas 
+       em você eu encontrei o que nem sabia que tava faltando em mim. Te amar me mostrou que vale a pena continuar 
+       vivendo e buscando coisas que dê vontade de viver (Por favor não se preocupe, se tu se afastar o sentimento
+       e a vontade de viver continuarão), aprendi a demonstrar mais o que sinto e até ser feliz, te amo de um jeito 
+       especial até pra mim. Esta não é uma carta de despedida, ta mais pra carta de amor, declaração de amor sei lá
+       enfim, saiba que eu te amo e muito e você valeria a pena qualquer coisa desse mundo, quero te ver sempre feliz.
+       Esse html deveria ser melhor tu merece o melhor mas infelizmente não consegui ajustar, no arquivo vai ter uma musica que
+       eu queria ter colocado para tocar junto mas se eu não conseguir vai ser só esta carta mesmo. Novamente ESTA NÃO É UMA CARTA DE
+       DESPEDIDA, e sim uma DECLARAÇÃO DE AMOR, infelizmente ainda não consigo por em palavras o quanto te amo, te adoro e te quero.
+       Enfim acho que já escrevi demais espero que eu consiga ficar do teu lado por muito tempo, te amo demais Janine Conceição dos Santos, minha jujubinha.</p>
+    <p>Com carinho,</p>
+    <p>SEU E SÓ SEU, Nerdiznho</p>
+  `;
+
+  letterContainer.innerHTML = carta;
+  letterContainer.style.display = "block";
+
+  
+}
